4fc4f2b62daef3fbb0216d7ff5990485e90dac2f
jananisenthilkumar291 <janani382001@gmail.com>
2026-07-22 22:12:30 -0400

    Cleanup

--- modules/charmed-airflow/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Copyright 2026 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-output "nifi_k8s" {
-  description = "NiFi application resource."
-  value       = module.nifi_k8s.application
-}
