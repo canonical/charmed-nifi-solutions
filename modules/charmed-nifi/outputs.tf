cdea3031bd25fa1553b00e55b183e7a4611b4fa2
jananisenthilkumar291 <60182776+jananisenthilkumar291@users.noreply.github.com>
2026-08-13 08:39:46 -0400

    [WF-532] feat: add optional git-integrator integration (#4)
    * Support git-intergator integration

--- modules/charmed-nifi/outputs.tf
+++ modules/charmed-nifi/outputs.tf
@@ -5,3 +5,8 @@ output "nifi_k8s" {
   description = "NiFi application resource."
   value       = module.nifi_k8s.application
 }
+
+output "git_integrator" {
+  description = "Git Integrator application resource (null when not enabled)."
+  value       = var.git_integrator.enabled ? module.git_integrator[0].application : null
+}
